4236792a15c3d426c1acb58158bc28bf9b88928c
Michael Blume <blume.mike@gmail.com>
2017-02-08 13:19:33 -0800

    move most code to platform independent file
    iOS build should work now?

--- index.android.js
+++ index.android.js
@@ -4,72 +4,7 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import HTMLView from 'react-native-htmlview';
-
-export default class Glowfic extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {thread: null};
-  }
-  render() {
-    console.log('rendering!');
-    if (this.state.thread === null) {
-      fetch('https://glowfic.com/api/v1/posts/190').
-        then(r => r.json()).
-        then(r => this.setState({thread: r})).
-        catch(e => console.warn(e));
-    }
-    return (
-      <View style={styles.container}>
-        <Text style={styles.instructions}>
-          {
-            (() => {
-              if (this.state.thread === null) {
-                return "no thread!";
-              } else {
-                return "found the thread! Got " + this.state.thread.data.replies.length + " tags!";
-              }
-            })()
-          }
-
-        </Text>
-        {(() => {
-          if (this.state.thread !== null) {
-            return <HTMLView
-              value={this.state.thread.data.content}
-            />;
-          }
-        })()}
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+import { AppRegistry } from 'react-native';
+import Glowfic from './glowfic';
 
 AppRegistry.registerComponent('Glowfic', () => Glowfic);
