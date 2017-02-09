bcafd7ba1e5bc40cc266fca35a66bc02cecc087c
Michael Blume <blume.mike@gmail.com>
2017-02-08 19:16:01 -0800

    display listview of tags
    just content, no users or icons

--- glowfic.js
+++ glowfic.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
+  ListView,
   View
 } from 'react-native';
 import HTMLView from 'react-native-htmlview';
+
+const tag = (rowData) => {
+  console.log('rendering a tag');
+  return <HTMLView
+    value={rowData.content}/>;
+};
+
+const Thread = ({thread}) => {
+  const ds = new ListView
+    .DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+    .cloneWithRows(thread);
+  return <ListView
+    dataSource={ds}
+    renderRow={tag}
+  />;
+};
 
 export default class Glowfic extends Component {
 
@@ -13,34 +30,26 @@ export default class Glowfic extends Component {
     this.state = {thread: null};
   }
   render() {
-    console.log('rendering!');
+    console.log('rendering!', this.state);
     if (this.state.thread === null) {
-      fetch('https://glowfic.com/api/v1/posts/190').
-        then(r => r.json()).
-        then(r => this.setState({thread: r})).
-        catch(e => console.warn(e));
+      fetch('https://glowfic.com/api/v1/posts/190').then((r) => r.json()).then((j) => {
+        console.log('got json', j);
+        const data = j.data;
+        const top_post = (({character, content, icon, user}) =>
+            ({id: 'toppost', character, content, icon, user}))(data);
+        console.log('top post', top_post);
+        const thread = [top_post, ...data.replies];
+        this.setState({thread});
+      }).catch(e => console.warn(e));
     }
     return (
       <View style={styles.container}>
-        <Text style={styles.instructions}>
-          {
-            (() => {
-              if (this.state.thread === null) {
-                return "no thread!";
-              } else {
-                return "found the thread! Got " + this.state.thread.data.replies.length + " tags!";
-              }
-            })()
-          }
+        {
+          this.state.thread === null ?
+            <Text>"no thread!"</Text> :
+            <Thread thread={this.state.thread}/>
+        }
 
-        </Text>
-        {(() => {
-          if (this.state.thread !== null) {
-            return <HTMLView
-              value={this.state.thread.data.content}
-            />;
-          }
-        })()}
       </View>
     );
   }
